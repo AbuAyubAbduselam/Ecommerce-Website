02a0d0cde64984664f47baaff52440d8bc1ce181
abuayubabduselam <abuayubabduselam@gmail.com>
2024-07-22 14:48:20 +0300

    first final commit

--- backend/app.js
+++ backend/app.js
@@ -4,17 +4,19 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: "https://ecommerce-website-abduselam.vercel.app/",
     credentials: true,
   })
 );
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/", (req, res) => {
+app.use("/", express.static(path.join(__dirname, "uploads")));
+app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
 
