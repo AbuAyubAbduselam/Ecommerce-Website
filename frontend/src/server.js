d4df10152bb362a7474a42a5cea01d42998b6cb4
abuayubabduselam <abuayubabduselam@gmail.com>
2024-07-22 13:34:24 +0300

    backend done

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://ecommerce-website-ten-orpin.vercel.app/api/v2";
 
 /* export const backend_url = "https://localhost:8000"; */
